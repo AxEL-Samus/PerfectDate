8de97d92ebf56d21589d230d2fca5b3199d67b94
Alexey <aleksey.aps.inc@gmail.com>
2023-02-19 20:23:51 +0300

    aborting redux

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,20 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 import {StyleSheet, Text, View} from 'react-native';
 import Navigation from './navigation';
+// import store from './src/redux/store';
 
 const App = () => {
   return (
+    // <BrowserRouter>
+    // <Provider store={store}>
     <NavigationContainer>
       <Navigation />
     </NavigationContainer>
+    // {/* </Provider> */}
+    // </BrowserRouter>
   );
 };
 
