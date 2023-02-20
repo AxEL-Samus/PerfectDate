15d82230223d598518f71f020856a1fee9b8a98d
Maksim Grushetskiy <mega_ymbetov@mail.ru>
2023-02-20 15:52:47 +0300

    made some style

--- client/src/screens/Modules.tsx
+++ client/src/screens/Modules.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Modal, View, StyleSheet, Text, Linking } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Modal, View, StyleSheet, Text, Linking, SafeAreaView } from 'react-native';
+import { Button, Card } from 'react-native-paper';
 import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function Modules({
@@ -16,136 +16,137 @@ export default function Modules({
   restUrl,
 }): JSX.Element {
   return (
-    <View>
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
+    <SafeAreaView
+      style={{
+        backgroundColor: 'rgba(0,0,0, 0.8)',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'rgba(0,0,0, 0.8)',
         }}
       >
-        <View
-          style={{
-            backgroundColor: 'rgba(0,0,0, 0.8)',
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 22,
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
           }}
         >
-          <View style={styles.map}>
-            <MapView
-              provider={PROVIDER_GOOGLE}
+          <View style={styles.container1}>
+            <Card
               style={{
-                height: '100%',
-                width: '100%',
-                borderRadius: 15,
-              }}
-              showsUserLocation="true"
-              region={{
-                latitude: coords.lat,
-                longitude: coords.lng,
-                latitudeDelta: 0.0374,
-                longitudeDelta: 0.0321,
+                borderRadius: 20,
               }}
             >
-              <Marker
-                coordinate={{
+              <MapView
+                provider={PROVIDER_GOOGLE}
+                style={{
+                  height: '50%',
+                  width: '100%',
+                  borderRadius: 15,
+                }}
+                showsUserLocation={true}
+                region={{
                   latitude: coords.lat,
                   longitude: coords.lng,
+                  latitudeDelta: 0.0374,
+                  longitudeDelta: 0.0321,
                 }}
-                pinColor="red"
-                fillColor="green"
-                strokeWidth={2}
-                title={`Тут рестороан: ${modalVariant}`}
-                onPress={() => Linking.openURL(restUrl)}
               >
-                <Callout>
-                  <Button>{`Тут рестороан: ${modalVariant}`}</Button>
-                </Callout>
-              </Marker>
-              <Marker
-                coordinate={{
-                  latitude: parksCoords.lat,
-                  longitude: parksCoords.lng,
-                }}
-                pinColor="white"
-                fillColor="green"
-                strokeWidth={2}
-                title={`Тут: ${park}`}
-              />
-              <Marker
-                coordinate={{
-                  latitude: kinoCoords.lat,
-                  longitude: kinoCoords.lng,
-                }}
-                title={`Тут кинотеатр: ${time.name}`}
-                description={`Cсылка на бронь: ${(<Button>Нажми</Button>)}`}
-                pinColor="blue"
-                // onCalloutPress={() => alert('Cсылка на бронь: ')}
-                onPress={() => Linking.openURL(kinoUrl)}
+                <Marker
+                  coordinate={{
+                    latitude: coords.lat,
+                    longitude: coords.lng,
+                  }}
+                  pinColor="red"
+                  title={`Тут рестороан: ${modalVariant}`}
+                  onPress={() => Linking.openURL(restUrl)}
+                >
+                  <Callout>
+                    <Button>{`Тут рестороан: ${modalVariant}`}</Button>
+                  </Callout>
+                </Marker>
+                <Marker
+                  coordinate={{
+                    latitude: parksCoords.lat,
+                    longitude: parksCoords.lng,
+                  }}
+                  pinColor="black"
+                  title="Тут парк"
+                />
+                <Marker
+                  coordinate={{
+                    latitude: kinoCoords.lat,
+                    longitude: kinoCoords.lng,
+                  }}
+                  title={`Тут кинотеатр: ${time.name}`}
+                  description={`Cсылка на бронь: ${(<Button>Нажми</Button>)}`}
+                  pinColor="blue"
+                  onPress={() => Linking.openURL(kinoUrl)}
+                >
+                  <Callout>
+                    <Button>{`Тут кинотеатр: ${time.name}`}</Button>
+                  </Callout>
+                </Marker>
+              </MapView>
+              <Text style={styles.paragraph}>{`Ресторан: ${modalVariant} (красная метка)`}</Text>
+              <Text style={styles.paragraph}>{`Парк: ${park} (черная метка)`}</Text>
+              {time.name === '' ? null : (
+                <Text style={styles.paragraph}>{`Кинотеатр: ${time.name} (синяя метка)`}</Text>
+              )}
+              {time.movie === '' ? null : (
+                <Text style={styles.paragraph}>{`Фильм: ${time.movie}`}</Text>
+              )}
+              {time.time === '' ? null : (
+                <Text style={styles.paragraph}>{`Время: ${time.time}`}</Text>
+              )}
+              <Button
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
               >
-                <Callout>
-                  <Button>{`Тут кинотеатр: ${time.name}`}</Button>
-                </Callout>
-              </Marker>
-            </MapView>
+                <Text style={styles.paragraph}>Закрыть</Text>
+              </Button>
+            </Card>
           </View>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>{`Ресторан: ${modalVariant} (красная метка)`}</Text>
-            <Text style={styles.modalText}>{`Парк: ${park} (черная метка)`}</Text>
-            {time.name && time.time === '' ? null : (
-              <>
-                <Text style={styles.modalText}>{`Кинотеатр: ${time.name} (синяя метка)`}</Text>
-                <Text style={styles.modalText}>{`Время сеанса: ${time.time}`}</Text>
-              </>
-            )}
-          </View>
-          <Button
-            style={[styles.button, styles.buttonClose]}
-            onPress={() => setModalVisible(!modalVisible)}
-          >
-            Закрыть
-          </Button>
-        </View>
-      </Modal>
-    </View>
+        </Modal>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  map: {
-    width: 220,
-    height: 230,
-    borderRadius: 10,
+  container1: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0, 0.6)',
+    borderRadius: 20,
   },
-  modalView: {
-    margin: 10,
-    backgroundColor: 'white',
-    borderRadius: 15,
-    padding: 50,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.65,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  buttonClose: {
-    backgroundColor: '#2196F3',
-  },
-  textStyle: {
-    color: 'white',
+  paragraph: {
+    fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+    padding: 18,
+    color: 'black',
   },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
+  img2: {
+    height: 220,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  header: {
+    paddingTop: '200px',
+    height: '100px',
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+  },
+  buttonClose: {
+    margin: 10,
+    backgroundColor: '#CCB188',
   },
 });
