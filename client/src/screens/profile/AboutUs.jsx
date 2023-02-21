8961ce1785aa6cbc708315fe65d227810c7bad81
Maksim Grushetskiy <mega_ymbetov@mail.ru>
2023-02-21 09:41:17 +0300

    made smth changes

--- client/src/screens/profile/AboutUs.jsx
+++ client/src/screens/profile/AboutUs.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default function AboutUs({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text
-        onPress={() => navigation.navigate("AboutUs")}
-        style={{ fontSize: 26, fontWeight: "bold" }}
+        onPress={() => navigation.navigate('AboutUs')}
+        style={{ fontSize: 26, fontWeight: 'bold' }}
       >
         About
       </Text>
