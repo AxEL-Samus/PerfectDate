3700dcf5bb5a2ea41a4051abe8f8dc34eb2692b5
AndreyNguen <goonikjke@gmail.com>
2023-02-22 14:29:36 +0300

    new commit

--- server/router/typesRouter.js
+++ server/router/typesRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { Types } = require('../db/models');
+const { Type, Attraction } = require('../db/models');
 
 const typesRouter = express.Router();
 
 typesRouter.get('/', async (req, res) => {
   try {
-    const alltypes = await Types.findAll();
+    const alltypes = await Type.findAll({ include: { model: Attraction } });
     console.log('00000000', alltypes);
     res.json(alltypes);
   } catch (error) {
